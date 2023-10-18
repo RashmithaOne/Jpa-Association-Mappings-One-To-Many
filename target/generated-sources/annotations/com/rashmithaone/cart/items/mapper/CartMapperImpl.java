06c443bd94f466899649043ac96fd88310f267c5
Rashmi <147464476+RashmithaOne@users.noreply.github.com>
2023-10-17 20:56:31 -0500

    Update CartMapperImpl.java

--- target/generated-sources/annotations/com/rashmithaone/cart/items/mapper/CartMapperImpl.java
+++ target/generated-sources/annotations/com/rashmithaone/cart/items/mapper/CartMapperImpl.java
@@ -1,18 +1,17 @@
 package com.rashmithaone.cart.items.mapper;
 
 import com.rashmithaone.cart.items.dto.CartDTO;
+import com.rashmithaone.cart.items.dto.CartItemsResponseBO;
 import com.rashmithaone.cart.items.entity.Cart;
 import com.rashmithaone.cart.items.entity.Item;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-10-11T22:51:54-0500",
+    date = "2023-10-17T20:22:04-0500",
     comments = "version: 1.4.1.Final, compiler: javac, environment: Java 17.0.8.1 (Eclipse Adoptium)"
 )
 @Component
@@ -29,9 +28,9 @@ public class CartMapperImpl implements CartMapper {
         cart.setCartId( cartDTO.getCartId() );
         cart.setName( cartDTO.getName() );
         cart.setDescription( cartDTO.getDescription() );
-        Set<Item> set = cartDTO.getItems();
-        if ( set != null ) {
-            cart.setItems( new HashSet<Item>( set ) );
+        List<Item> list = cartDTO.getItems();
+        if ( list != null ) {
+            cart.setItems( new ArrayList<Item>( list ) );
         }
 
         return cart;
@@ -48,9 +47,9 @@ public class CartMapperImpl implements CartMapper {
         cartDTO.setCartId( cart.getCartId() );
         cartDTO.setName( cart.getName() );
         cartDTO.setDescription( cart.getDescription() );
-        Set<Item> set = cart.getItems();
-        if ( set != null ) {
-            cartDTO.setItems( new HashSet<Item>( set ) );
+        List<Item> list = cart.getItems();
+        if ( list != null ) {
+            cartDTO.setItems( new ArrayList<Item>( list ) );
         }
 
         return cartDTO;
@@ -65,6 +64,20 @@ public class CartMapperImpl implements CartMapper {
         List<CartDTO> list = new ArrayList<CartDTO>( carts.size() );
         for ( Cart cart : carts ) {
             list.add( entityToDto( cart ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public List<CartItemsResponseBO> cartItemsToDto(List<CartItemsResponseBO> carts) {
+        if ( carts == null ) {
+            return null;
+        }
+
+        List<CartItemsResponseBO> list = new ArrayList<CartItemsResponseBO>( carts.size() );
+        for ( CartItemsResponseBO cartItemsResponseBO : carts ) {
+            list.add( cartItemsResponseBO );
         }
 
         return list;
