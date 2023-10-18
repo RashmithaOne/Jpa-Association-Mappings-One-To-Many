76bf56ec1d0031aeb942a349ca09ca53d38d0f68
Rashmi <147464476+RashmithaOne@users.noreply.github.com>
2023-10-17 20:38:27 -0500

    Update CartRepository.java

--- src/main/java/com/rashmithaone/cart/items/repository/CartRepository.java
+++ src/main/java/com/rashmithaone/cart/items/repository/CartRepository.java
@@ -1,9 +1,17 @@
 package com.rashmithaone.cart.items.repository;
 
+import com.rashmithaone.cart.items.dto.CartItemsResponseBO;
 import com.rashmithaone.cart.items.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Integer> {
+  
+    // JPQL query to pull the cart items and join the cart and item tables with the return type - CartItemsResponseBO
+    @Query(value = "SELECT new com.rashmithaone.cart.items.dto.CartItemsResponseBO(c.name, i.name, i.itemDetails, i.brand, i.quantity) FROM Cart c JOIN c.items i")
+    public List<CartItemsResponseBO> getAllCartItemsCustomDetails();
 }
