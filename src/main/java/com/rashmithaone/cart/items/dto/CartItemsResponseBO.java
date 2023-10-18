87004e9191b06fce7573ae86d1de6bf9f01c5294
Rashmi <147464476+RashmithaOne@users.noreply.github.com>
2023-10-17 20:36:03 -0500

    Create CartItemsResponseBO.java

---
+++ src/main/java/com/rashmithaone/cart/items/dto/CartItemsResponseBO.java
@@ -0,0 +1,20 @@
+package com.rashmithaone.cart.items.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+public class CartItemsResponseBO {
+  
+    // name from Cart entity
+    private String name; 
+    
+    // fields from Item entity
+    private String itemName; // 
+    private String itemDetails;
+    private String brand;
+    private int quantity;
+}
